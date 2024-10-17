594525465c078fd275bd94676609e1b9ac52ef21
renanppcosta <renanppcosta@gmail.com>
2024-10-17 17:46:11 -0300

    feat: updated settings and added requirements.txt with dependencies

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
 
-from tasks.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', include('tasks.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
