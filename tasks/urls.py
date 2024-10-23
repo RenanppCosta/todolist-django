feb7e21025f055613e47eb061f7daa45ccece9da
Renanppcosta <renanppcosta@gmail.com>
2024-10-22 23:33:45 -0300

    feat: implemention of update task with ajax

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,6 @@ from tasks import views
 
 urlpatterns = [
     path('', views.create_task, name='create_task'),
-    path('delete/<int:id>/', views.delete_task, name='delete_task')
+    path('delete/<int:id>/', views.delete_task, name='delete_task'),
+    path('update', views.update_task, name='update_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
 from .models import Task  
 from .forms import TaskForm  
 
@@ -18,3 +19,14 @@ def delete_task(request, id):
     task = Task.objects.get(id=id)
     task.delete()
     return redirect('create_task')
+
+def update_task(request):
+        data_id = request.GET.get("data_id")
+        title = request.GET.get("task_title")
+
+        task = get_object_or_404(Task, id=data_id)
+        task.title = title
+        task.save()
+
+        data = {"title": title, "status":"update_task"}
+        return JsonResponse(data)
